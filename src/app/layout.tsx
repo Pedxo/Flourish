f2ae1598a7d88f2ffd12f0b68c082a9c47693091
Victor Chukwuma <114806682+Vikxlife@users.noreply.github.com>
2026-04-01 00:18:13 +0100

    Initial commit: Flourish minimalist website (#1)
    Co-authored-by: Vikxx <vikxx@Vikxxs-MacBook-Air.local>

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { EB_Garamond } from "next/font/google";
+import "./globals.css";
+
+const garamond = EB_Garamond({ 
+  subsets: ["latin"],
+  variable: '--font-serif',
+  weight: ['400', '500', '600']
+});
+
+export const metadata: Metadata = {
+  title: "Flourish- Design and creative collective for industries",
+  description: "Flourish is a creative collective.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${garamond.variable} antialiased selection:bg-black selection:text-white`}>
+        {children}
+      </body>
+    </html>
+  );
+}
