f7c7108be329dbdd2b7f2995589b924395304233
oritse_dev <ikareemabiodun730@gmail.com>
2026-08-01 09:32:17 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { EB_Garamond } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 
 const garamond = EB_Garamond({ 
   subsets: ["latin"],
